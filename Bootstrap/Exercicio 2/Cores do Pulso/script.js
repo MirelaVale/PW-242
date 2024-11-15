88a99ff6d81c5ecfad018955a5967a8d8d2d3e7c
Mirela <141439795+MirelaVale@users.noreply.github.com>
2024-11-14 23:39:34 -0300

    Delete Bootstrap/Exercicio 2/Cores do Pulso directory

--- Bootstrap/Exercicio 2/Cores do Pulso/script.js
+++
@@ -1,8 +0,0 @@
-function adicionarAoCarrinho(produto) {
-    alert(`${produto} foi adicionado ao carrinho!`);
-}
-
-document.getElementById('formContato').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Formulário enviado com sucesso!');
-});
